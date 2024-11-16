d6c5ca0a8f311995ef62f8d5a72daf50569cd116
nıdaturkay <145764089+NdaTrky@users.noreply.github.com>
2024-11-16 18:28:20 +0300

    Day 3 görevi tamamlandı.

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,45 @@
+package org.example;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    double salary;
+    boolean isMarried;
+    String[] hobbies;
+// bir class da; birden fazla constructor olmasına constructor overloading diyebiliriz.
+// 1. Constructor
+    public Person(String firstName, String lastName, int age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+// 2. Constructor
+    public Person(String firstName, String lastName, int age, double salary, boolean isMarried, String[] hobbies){
+        this(firstName, lastName,age); // constructor chaining
+        this.salary = salary;
+        this.isMarried = isMarried;
+        this.hobbies = hobbies;
+
+    }
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public boolean isTeen(){
+       return this.age >= 13 && this.age <= 19;
+    }
+
+
+}
+
+
